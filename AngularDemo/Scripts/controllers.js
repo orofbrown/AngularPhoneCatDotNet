e95b83582c6732ec52ea9ce084d999bf262c02b6
Mike Young <mike.young@zsassociates.com>
2015-09-02 11:42:32 -0500

    Missing curly brace

--- AngularDemo/Scripts/controllers.js
+++ AngularDemo/Scripts/controllers.js
@@ -5,13 +5,13 @@
 var phonecatControllers = angular.module('phonecatControllers', []);
 
 phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
-    function ($scope, Phone) {
+    function($scope, Phone) {
         $scope.phones = Phone.query();
         $scope.orderProp = 'age';
         $scope.detailView = false;
 
-        $scope.getDetails = function (id) {
-            $scope.phone = Phone.get({ phoneId: id }, function (phone) {
+        $scope.getDetails = function(id) {
+            $scope.phone = Phone.get({ phoneId: id }, function(phone) {
                 $scope.mainImageUrl = phone.images[0];
             });
             $scope.detailView = true;
@@ -19,10 +19,10 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
 
         $scope.getList = function() {
             $scope.detailView = false;
-            console.log($scope.phones);
         }
 
-        $scope.setImage = function (imageUrl) {
+        $scope.setImage = function(imageUrl) {
             $scope.mainImageUrl = imageUrl;
         };
+    }
 ]);
